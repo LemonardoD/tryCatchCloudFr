bbf9ed537ad1ea4293109292daf342fd5bd84992
David Hordiienko <neonz96@mail.ru>
2023-11-20 14:51:11 +0200

    Change gitHub url

--- src/routes/getIdToken/+page.ts
+++ src/routes/getIdToken/+page.ts
@@ -1,12 +1,10 @@
 import Cookies from "js-cookie";
 import type { PageLoad } from "./$types";
 
-const { VITE_API_URL } = import.meta.env;
-
 export const load: PageLoad = ({ fetch, url }) => {
 	const code = url.searchParams.get("code");
 	const fetchApi = async () => {
-		const apiResponse = await fetch(`https://${VITE_API_URL}/api/login/getUserinfo?code=${code}`, {
+		const apiResponse = await fetch(`https://trycatchcloud.fly.dev/api/login/getUserinfo?code=${code}`, {
 			method: "GET",
 			mode: "cors",
 			headers: {
@@ -16,7 +14,7 @@ export const load: PageLoad = ({ fetch, url }) => {
 			referrerPolicy: "no-referrer",
 		});
 		const data = await apiResponse.json();
-		Cookies.set("jwt", data.token);
+		Cookies.set("jwt", data.jwtToken);
 		return data;
 	};
 	return fetchApi();
