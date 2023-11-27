ad1b42e4a0d71bd821cfd8f1f3dd303f2464d334
David Hordiienko <neonz96@mail.ru>
2023-11-27 15:01:39 +0200

    change style

--- src/routes/(nav-bar-pages)/show-token/+page.svelte
+++ src/routes/(nav-bar-pages)/show-token/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-    import copyIcon from "../../lib/icons/copy.png"
+    import copyIcon from "../../../lib/icons/copy.png"
     import Loader from "../../../components/loader.svelte";
 
     export let data
